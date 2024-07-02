76753ee801fe3975d61e68d0de256ed9dfa96ecd
Liam Houghton <liamhoughton@192.168.1.225>
2024-07-02 22:50:40 +0100

    add error handling if user not found

--- models/users.models.js
+++ models/users.models.js
@@ -1,31 +1,48 @@
-const { client } = require('../db/connection')
+const { client } = require("../db/connection");
 
 exports.createUser = async (username, password, isAdmin) => {
-    try {
-        const db = client.db('ChatPFQ')
-        const collection = db.collection('users')
-        const insertedUser = await collection.insertOne({
-            username,
-            password, 
-            isAdmin
-        })
-        return insertedUser
-    } catch (error) {
-       throw error
-    }
-}
+  try {
+    const db = client.db("ChatPFQ");
+    const collection = db.collection("users");
+    const insertedUser = await collection.insertOne({
+      username,
+      password,
+      isAdmin,
+    });
+    return insertedUser;
+  } catch (error) {
+    return Promise.reject(error)
+  }
+};
 
 exports.fetchUsers = async () => {
-    try {
-        const db = client.db('ChatPFQ');
-        const collection = db.collection('users');
-        const results = collection.find({});
-        return await results.toArray()
-    } catch (error) {
-       throw error
-    }
-}
+  try {
+    const db = client.db("ChatPFQ");
+    const collection = db.collection("users");
+    const results = collection.find({});
+    return await results.toArray();
+  } catch (error) {
+    return Promise.reject(error)
+  }
+};
 
-exports.fetchUserById = async () => {
-    
-}
+exports.fetchUserById = async (username) => {
+  try {
+    const db = client.db("ChatPFQ");
+    const collection = db.collection("users");
+    const result = await collection
+      .find({
+        username: username,
+      })
+      .toArray();
+    if (result.length === 0) {
+      return Promise.reject({
+        status: 404,
+        msg: "User not found"
+      });
+    }
+    return result[0];
+  } catch (error) {
+    return Promise.reject(error)
+  }
+};
