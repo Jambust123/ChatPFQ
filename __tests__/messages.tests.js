e5ed5295f27015c77fc80eb18af074e5b5f68ed6
Jake Slattery <alexander.slattery@googlemail.com>
2024-07-02 16:46:16 +0100

    added /get/messages?usernames=:usernames endpoint with full TDD

--- __tests__/messages.tests.js
+++ __tests__/messages.tests.js
@@ -36,24 +36,70 @@ describe("postMessage", () => {
   });
 });
 
-describe("getMessage", () => {
-  it("should 200 retrun all users when requested", async () => {
-    const { body } = await request(app)
-    .get("/api/messages")
-    .expect(200);
+describe("getAllMessages", () => {
+  it("should 200 retrun all messages when requested", async () => {
+    const { body } = await request(app).get("/api/messages").expect(200);
 
     expect(body).toHaveLength(100);
     body.forEach((user) => {
       expect(user).toMatchObject({
-          _id: expect.any(String),
-          body: expect.any(String),
-          from: expect.any(String),
-          to: "admin",
-          category: 'Service',
-          sentiment: "negative",
-          isClosed: expect.any(Boolean),
-          table: expect.any(Number),
+        _id: expect.any(String),
+        body: expect.any(String),
+        from: expect.any(String),
+        to: "admin",
+        category: "Service",
+        sentiment: "negative",
+        isClosed: expect.any(Boolean),
+        table: expect.any(Number),
       });
     });
   });
 });
+
+describe("getAllMessagesFromUser", () => {
+  it("should 200 should return all messages from a specific user", async () => {
+    const input = {
+      message: {
+        body: `This is a test message`,
+        from: "matt",
+        to: "admin",
+        category: "Service",
+        sentiment: "negative",
+        isClosed: false,
+        table: 12,
+      },
+    };
+
+    const input2 = {
+      message: {
+        body: `This is another test message`,
+        from: "admin",
+        to: "matt",
+        category: "Service",
+        sentiment: "negative",
+        isClosed: false,
+        table: 12,
+      },
+    };
+
+    await request(app).post("/api/messages").send(input);
+    await request(app).post("/api/messages").send(input2);
+
+    const { body } = await request(app).get("/api/messages?username=matt").expect(200);
+
+    body.forEach((user) => {
+      expect(user).toMatchObject({
+        _id: expect.any(String),
+        body: expect.any(String),
+        from: expect.any(String),
+        to: expect.any(String),
+        category: "Service",
+        sentiment: "negative",
+        isClosed: false,
+        table: 12,
+      });
+    });
+  });
+});
+
+
