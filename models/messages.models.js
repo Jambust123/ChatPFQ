51d2b8ca353365b67d9a7e7346219613ef7639eb
Liam Houghton <liamhoughton@Liams-MBP.northcoders.com>
2024-07-03 16:32:29 +0100

    unwrap message from from messages input

--- models/messages.models.js
+++ models/messages.models.js
@@ -1,20 +1,19 @@
 const { ObjectId } = require("mongodb");
 const { connect, getClient, close } = require("../db/connection");
 
-exports.createMessage = async (message) => {
+exports.createMessage = async (body, from, to, category, sentiment, isClosed, table) => {
   try {
     const client = await connect()
     const db = client.db("ChatPFQ");
     const collection = db.collection("messages");
     const insertedMessage = await collection.insertOne({
-      body: JSON.stringify(message.body),
-      from: message.from,
-      to: message.to,
-      created_at: new Date(),
-      category: message.category,
-      sentiment: message.sentiment,
-      isClosed: message.isClosed,
-      table: message.table,
+      body,
+      from,
+      to,
+      category,
+      sentiment,
+      isClosed,
+      table,
     });
     return insertedMessage;
   } catch (error) {
